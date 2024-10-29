afdd4082d70fc02d1525bae72956ab4ef7d66ac2
Javier González <javier.gonzalez2101@alumnos.ubiobio.cl>
2024-10-28 22:02:43 -0300

    MOD index.routes.js

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -4,12 +4,14 @@ import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import estadisticasRoutes from "./estadisticas.routes.js";
 import invRoutes from "./inventario.routes.js";
+import bicicletaRoutes from "./bicicleta.routes.js";
 
 const router = Router();
 
 router
     .use("/auth", authRoutes)
     .use("/user", userRoutes)
+    .use("/bicicleta", bicicletaRoutes)
     .use("/estadisticas", estadisticasRoutes)
     .use("/inventario", invRoutes);
 
