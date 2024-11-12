dbb59509ba33fcac7da4f0d2c6d35ba5e269df1d
Javier González <javier.gonzalez2101@alumnos.ubiobio.cl>
2024-11-12 00:28:26 -0300

    MOD: Modificaciones a la página de inicio

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 const Home = () => {
   return (
-    <>
-    </>
+    <div className='main-container'>
+      <div className='swal2-info'>
+        <h1 className='swal2-title'>¡Bienvenido!</h1>
+        <div className='warning'>
+          <p>Sitio aún en construcción</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
