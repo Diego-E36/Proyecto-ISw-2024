805045e712109d00b3436bc123362c6b6d6c02cc
Ignacio Mora <ignacio.mora2001@alumnos.ubiobio.cl>
2024-11-11 23:06:25 -0300

    MOD: cambios al formateo de los parametros

--- frontend/src/helpers/formatInventario.js
+++ frontend/src/helpers/formatInventario.js
@@ -1,17 +1,22 @@
 import { startCase } from "lodash";
 import { format as formatTempo } from "@formkit/tempo";
 import { formatPostUpdate } from "./formatData";
+import { useMemo } from "react";
 
 export function formatInventarioData(inventario) {
     return {
         ...inventario,
         numeroSerie: inventario.numeroSerie,
         nombreStock: startCase(inventario.nombreStock),
-        colorUnidad: startCase(inventario.colorUnidad),
+        descripcionUnidad: startCase(inventario.descripcionUnidad),
         precioUnidad: inventario.precioUnidad,
         marcaUnidad: startCase(inventario.marcaUnidad),
-        createdAt: formatTempo(inventario.createdAt, "DD-MM-YYYY"),
-        updatedAt: formatTempo(inventario.updatedAt, "DD-MM-YYYY"),
+        proveedor: startCase(inventario.proveedor),
+        restockSugerido: inventario.restockSugerido,
+        umbralMinimo: inventario.umbralMinimo,
+        boolMateriales: startCase(inventario.boolMateriales),
+        createdAt: formatTempo(inventario.createdAt, "DD-MM-YYYY HH:mm"),
+        updatedAt: formatTempo(inventario.updatedAt, "DD-MM-YYYY HH:mm"),
     };
 }
 
@@ -19,11 +24,15 @@ export function formatPostInventario(inventario) {
     return {
         numeroSerie: inventario.numeroSerie,
         nombreStock: startCase(inventario.nombreStock),
-        colorUnidad: startCase(inventario.colorUnidad),
+        descripcionUnidad: startCase(inventario.descripcionUnidad),
         precioUnidad: inventario.precioUnidad,
         marcaUnidad: startCase(inventario.marcaUnidad),
-        createdAt: formatTempo(inventario.createdAt, "DD-MM-YYYY"),
-        updatedAt: formatTempo(inventario.updatedAt, "DD-MM-YYYY"),
+        proveedor: startCase(inventario.proveedor),
+        restockSugerido: inventario.restockSugerido,
+        umbralMinimo: inventario.umbralMinimo,
+        boolMateriales: inventario.boolMateriales,
+        createdAt: formatTempo(inventario.createdAt, "DD-MM-YYYY HH:mm"),
+        updatedAt: formatTempo(inventario.updatedAt, "DD-MM-YYYY HH:mm"),
     };
 }
 
