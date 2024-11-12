69676f725550ff19e7f7c887f4f275e7a8f38a88
Diego-E36 <diego.gonzalez2101@alumnos.ubiobio.cl>
2024-11-12 19:03:07 -0300

    ADD notificaciones controller

---
+++ backend/src/controllers/notificaciones.controller.js
@@ -0,0 +1,41 @@
+"use strict";
+import {
+    deleteNotificationService,
+    getAllNotificationsService   
+} from "../services/notificaciones.service.js";
+
+import {
+    handleErrorClient,
+    handleErrorServer,
+    handleSuccess,
+} from "../handlers/responseHandlers.js";
+
+
+export async function deleteNotification(req, res) {
+    try {
+        const { id } = req.params;
+        
+        const [notiDelete, errorNoti] = await deleteNotificationService({ id });
+
+        if (errorNoti) return handleErrorClient(res, 404, "Error eliminando notificación", errorNoti);
+        
+        handleSuccess(res, 200, "Notificación eliminada correctamente", notiDelete);
+    } catch (error) {
+        handleErrorServer(res, 500, error.message);
+    }
+}
+
+export async function getAllNotifications(req, res) {
+    try {
+        const [notificacion, errorNoti] = await getAllNotificationsService();
+
+        if (errorNoti) return handleErrorClient(res, 404, errorNoti);
+
+        notificacion.length === 0
+            ? handleSuccess(res, 204)
+            : handleSuccess(res, 200, "Notificaciones encontrada", notificacion);
+    } catch (error) {
+        handleErrorServer(res, 500, error.message);
+    }
+}
+
