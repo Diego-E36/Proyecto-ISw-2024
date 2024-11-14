3ad720e047de7cdd1a099189085f5d903dec1090
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-11-13 23:41:43 -0300

    MOD import estadisticasRoutes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,7 +2,7 @@
 import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
-import estadisticasRoutes from "./estadisticas.routes.js";
+import estadisticasRoutes from "./estadisticasInv.routes.js";
 import invRoutes from "./inventario.routes.js";
 import bicicletaRoutes from "./bicicleta.routes.js";
 import materialesRoutes from "./materiales.routes.js";
