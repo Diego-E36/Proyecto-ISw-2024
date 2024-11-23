847caf82caf542b4b5b7fc62c4122b3ad04dcd63
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-11-23 01:24:54 -0300

    MOD estadisticasInv.controller.js

--- backend/src/controllers/estadisticasInv.controller.js
+++ backend/src/controllers/estadisticasInv.controller.js
@@ -1,9 +1,9 @@
 "use strict";
-import { getNombreYCantidadInventario } from "../services/estadisticasInv.service.js";
+import { getDistribucionProductosPorProveedor, getNombreYCantidadInventario, getProductosBajoStockYRestockSugerido } from "../services/estadisticasInv.service.js";
 import { handleErrorClient, handleErrorServer, handleSuccess } from "../handlers/responseHandlers.js";
 //Aquí van las cosas para manejar las peticiones HTTP
 
-// Controlador para obtener el nombre del producto y la cantidad
+// Obtener el nombre del producto y la cantidad
 export async function getNombreYCantidadInventarioController(req, res) {
     try {
         const [inventario, error] = await getNombreYCantidadInventario();
@@ -17,6 +17,41 @@ export async function getNombreYCantidadInventarioController(req, res) {
         }
 
         handleSuccess(res, 200, "Nombre y cantidad del inventario obtenidos con éxito", inventario);
+    } catch (error) {
+        handleErrorServer(res, 500, "Error interno del servidor", error);
+    }
+}
+
+// Obtener la distribución de productos por proveedor
+export async function getDistribucionProductosPorProveedorController(req, res) {
+    try {
+        const [productosPorProveedor, error] = await getDistribucionProductosPorProveedor();
+
+        if (error) {
+            return handleErrorClient(res, 404, "Error al obtener la distribución de productos por proveedor", error);
+        }
+
+        handleSuccess(res, 200, "Distribución de productos por proveedor obtenida con éxito", productosPorProveedor);
+    } catch (error) {
+        handleErrorServer(res, 500, "Error interno del servidor", error);
+    }
+}
+
+
+// Obtener productos con bajo stock y restock sugerido
+export async function getProductosBajoStockYRestockSugeridoController(req, res) {
+    try {
+        const [productosBajoStock, error] = await getProductosBajoStockYRestockSugerido();
+
+        if (error) {
+            return handleErrorClient(res, 404, "Error al obtener productos con bajo stock y restock sugerido", error);
+        }
+
+        if (!productosBajoStock || productosBajoStock.length === 0) {
+            return handleSuccess(res, 204, "No hay productos con bajo stock");
+        }
+
+        handleSuccess(res, 200, "Productos con bajo stock y restock sugerido obtenidos con éxito", productosBajoStock);
     } catch (error) {
         handleErrorServer(res, 500, "Error interno del servidor", error);
     }
