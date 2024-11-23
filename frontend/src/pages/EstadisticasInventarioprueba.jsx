f738a7b7d235a92eb2d11cbd22c4f2e87536f32b
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-11-23 01:26:47 -0300

    MOD EstadisticasInventarioprueba.jsx

--- frontend/src/pages/EstadisticasInventarioprueba.jsx
+++ frontend/src/pages/EstadisticasInventarioprueba.jsx
@@ -1,11 +1,11 @@
 //import React from 'react';
-import EstadisticasInventarioChart from '../components/EstadisticasInventarioChart';
+import EstadisticasInventarioChart from '../components/EstadisticasInventarioChart.jsx';
 
 const EstadisticasInventarioprueba = () => {
     return (
-        <div className='main-container' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <div className='table-container' style={{ width: '80%' }}>
-                <div className='top-table'>
+        <div className='main-container' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: '20px', paddingTop: '20px', heigh: '100vh' }}>
+            <div className='table-container' style={{ width: '80%'}}>
+                <div className='top-table' style={{ marginBottom: '20px' }}>
                     <h1 className='title-table' style={{ textAlign: 'center' }}>Estadísticas del Inventario</h1>
                 </div>
                 <EstadisticasInventarioChart />
