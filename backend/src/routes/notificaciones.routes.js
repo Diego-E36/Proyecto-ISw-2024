f73eb457c6dbab1b72554b0b1c21ee2941e37d4a
Diego-E36 <diego.gonzalez2101@alumnos.ubiobio.cl>
2024-11-27 18:14:57 -0300

    MOD admin validation

--- backend/src/routes/notificaciones.routes.js
+++ backend/src/routes/notificaciones.routes.js
@@ -1,6 +1,8 @@
 "use strict";
 // Importar Router desde express
 import { Router } from "express";
+import { isAdmin } from "../middlewares/authorization.middleware.js";
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
 // Importar manejo de inventario desde controladores
 import {
     deleteNotification,
@@ -11,7 +13,8 @@ const router = Router();
 
 
 router
-    
+    .use(authenticateJwt) // Este middleware se aplica a todas las rutas
+    .use(isAdmin) // A partir de aquí, solo los administradores
     .get("/all", getAllNotifications)
     .delete("/:id", deleteNotification);
 
