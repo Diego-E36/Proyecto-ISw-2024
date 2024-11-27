d6ef16e817d507d5b8ab770194192724471859ef
Diego-E36 <diego.gonzalez2101@alumnos.ubiobio.cl>
2024-11-27 18:13:57 -0300

    MOD entity

--- backend/src/entity/notificaciones.entity.js
+++ backend/src/entity/notificaciones.entity.js
@@ -31,7 +31,7 @@ const notificationesSchema = new EntitySchema({
             default: () => "CURRENT_TIMESTAMP",
             nullable: false,
         },      
-    }, 
+    }
 });
 
 export default notificationesSchema;
