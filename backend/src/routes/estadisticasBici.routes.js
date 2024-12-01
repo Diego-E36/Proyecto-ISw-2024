1cdf52b19425298e437bf9732b806a993acdf314
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-12-01 01:54:14 -0300

    ADD estadisticasBici.routes.js

---
+++ backend/src/routes/estadisticasBici.routes.js
@@ -0,0 +1,18 @@
+"use strict";
+import { Router } from "express";
+import { getBicicletasPorTipoController } from "../controllers/estadisticasBici.controller.js";
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
+import { isAdmin } from "../middlewares/authorization.middleware.js";
+
+const router = Router();
+
+// Aplica middleware de autenticación y autorización
+router
+    .use(authenticateJwt)
+    .use(isAdmin);
+
+// Define rutas para las estadísticas de bicicletas
+router
+    .get("/tipobici", getBicicletasPorTipoController);
+
+export default router;
