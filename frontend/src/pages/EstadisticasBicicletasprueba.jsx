e7b989cb1dcf8b00ba1c9bb494956d3840ed3f00
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-12-04 01:53:53 -0300

    MOD EstadisticasBicicletasprueba.jsx

--- frontend/src/pages/EstadisticasBicicletasprueba.jsx
+++ frontend/src/pages/EstadisticasBicicletasprueba.jsx
@@ -3,11 +3,17 @@ import EstadisticasBicicletasChart from '../components/EstadisticasBicicletasCha
 
 const EstadisticasBicicletasprueba = () => {
     return (
-        <div className='main-container' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: '20px', paddingTop: '20px', height: '100vh' }}>
-            <div className='table-container' style={{ width: '80%' }}>
-                <div className='top-table' style={{ marginBottom: '20px' }}>
-                    <h1 className='title-table' style={{ textAlign: 'center' }}>Estadísticas de Bicicletas</h1>
-                </div>
+        <div
+            className='main-container'
+            style={{
+                display: 'grid',
+                gridTemplateRows: 'auto 1fr', // Fila superior para el título, la segunda crece dinámicamente
+                minHeight: '100vh',
+            }}
+        >
+            <div className='top-table' style={{ marginBottom: '20px', textAlign: 'center' }}>
+            </div>
+            <div className='table-container' style={{ width: '97.4%', margin: '0' }}>
                 <EstadisticasBicicletasChart />
             </div>
         </div>
