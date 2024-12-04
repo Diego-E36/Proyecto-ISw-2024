e68f6d25021e15610da3ef036e8e98bd347d944b
Diego-E36 <diego.gonzalez2101@alumnos.ubiobio.cl>
2024-12-04 01:17:41 -0300

    ADD servicio routes

---
+++ backend/src/routes/servicio.routes.js
@@ -0,0 +1,26 @@
+"use strict";
+// Importar Router desde express
+import { Router } from "express";
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
+// Importar manejo de inventario desde controladores
+import {
+    createServicio,
+    deleteServicio,
+    getAllServicios,
+    getServicio,
+    updateServicio 
+} from "../controllers/servicio.controller.js";
+import { deleteInvService } from "../services/inventario.service.js";
+
+const router = Router();
+
+
+router
+    .use(authenticateJwt) // Este middleware se aplica a todas las rutas
+    .post("/", createServicio)
+    .delete("/:id", deleteServicio)
+    .get("/", getAllServicios)
+    .get("/:id_bicicleta",  getServicio)
+    .patch("/:id", updateServicio);
+
+export default router;
