c9eb37477d7a426a6c737b91d32b116ef7162357
Javier González <javier.gonzalez2101@alumnos.ubiobio.cl>
2024-12-06 22:43:47 -0300

    MOD: Añade verificación de rol de usuario en Home. Si el usuario es un "invitado", se muestra un mensaje informativo pidiendo esperar la asignación de un rol válido.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,28 @@
 const Home = () => {
-  return (
-    <div className='main-container'>
-      <div className='swal2-info'>
-        <h1 className='swal2-title'>¡Bienvenido!</h1>
-        <div className='warning'>
-          <p>Sitio aún en construcción</p>
-        </div>
+
+    const user = JSON.parse(sessionStorage.getItem('usuario')) || '';
+    const userRole = user?.rol;
+
+    if(userRole === 'invitado'){
+        return (
+            <div className='main-container'>
+                <div className='swal2-info'>
+                    <h1 className='swal2-title' style={{textAlign: 'center'}}>¡Bienvenido!</h1>
+                    <div className='warning'>
+                        <p>Espera a que el administrador del sistema te asigne un rol válido.</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className='main-container'>
+            <div className='swal2-info'>
+                <h1 className='swal2-title'>¡Bienvenido!</h1>
+                <div className='warning'>
+                    <p>Sitio aún en construcción</p>
+                </div>
       </div>
     </div>
   )
