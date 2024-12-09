590e9774debdaa584668fed6a4a12a48d722dd51
Diego-E36 <diego.gonzalez2101@alumnos.ubiobio.cl>
2024-12-08 21:46:20 -0300

    MOD controller de update

--- backend/src/controllers/servicio.controller.js
+++ backend/src/controllers/servicio.controller.js
@@ -1,9 +1,7 @@
 "use strict";
 import {
     createServicioService,
-    deleteServicioService,
     getAllServiciosService,
-    getServicioService,
     updateServicioService,
 } from "../services/servicio.service.js";
 
@@ -17,39 +15,6 @@ import {
     servicioBodySchema,
     servicioQuerySchema
 } from "../validations/servicio.validation.js";
-
-export async function getServicio(req, res){
-    try{
-        const { id } = req.params;
-        const { id_bicicleta, id_usuario } = req.query;
-
-        const { error } = servicioQuerySchema.validate({ id, id_bicicleta, id_usuario });
-
-        if(error) return handleErrorClient(res, 400, "Error de validación en la consulta", error.message);
-
-        const [servicio, errorServicio] = await getServicioService({ id, id_bicicleta, id_usuario });
-
-        if(errorServicio) return handleErrorClient(res, 404, errorServicio);
-
-        handleSuccess(res, 200, "Servicio encontrado", servicio);
-    }catch(error){
-        handleErrorServer(res, 500, error.message);
-    }
-}
-
-export async function getAllServicios(req, res){
-    try{
-        const [servicios, errorServicios] = await getAllServiciosService();
-
-        if(errorServicios) return handleErrorClient(res, 404, errorServicios);
-
-        servicios.length === 0
-            ? handleSuccess(res, 204, "No hay servicios registrados", {})
-            : handleSuccess(res, 200, "Servicios encontrados", servicios);
-    }catch(error){
-        handleErrorServer(res, 500, error.message);
-    }
-}
 
 export async function createServicio(req, res){
     try{
@@ -69,20 +34,33 @@ export async function createServicio(req, res){
     }
 }
 
+export async function getAllServicios(req, res){
+    try{
+        const [servicios, errorServicios] = await getAllServiciosService();
+
+        if(errorServicios) return handleErrorClient(res, 404, errorServicios);
+
+        servicios.length === 0
+            ? handleSuccess(res, 204, "No hay servicios registrados", {}) : handleSuccess(res, 200, "Servicios encontrados", servicios);
+    }catch(error){
+        handleErrorServer(res, 500, error.message);
+    }
+}
+
 export async function updateServicio(req, res){
     try{
-        const { id, id_bicicleta, id_usuario } = req.query;
+        const { id } = req.params;
         const { body } = req;
 
         const { error: queryError } = servicioBodySchema.validate(body);
 
         if(queryError) return handleErrorClient(res, 400, "Error de validación en la consulta", queryError.message);
 
-        const { error: bodyError } = servicioQuerySchema.validate({ id, id_bicicleta, id_usuario });
+        const { error: bodyError } = servicioQuerySchema.validate( { id } );
 
-        if(bodyError) return handleErrorClient(res, 400, "Error de validación en los datos enviados", bodyError.message);
+        if(bodyError) return handleErrorClient(res, 400, "Error de validación en los datos enviados aaa", bodyError.message);
 
-        const [servicio, errorServicio] = await updateServicioService({ id, id_bicicleta, id_usuario }, body);
+        const [servicio, errorServicio] = await updateServicioService({ id }, body);
 
         if(errorServicio) return handleErrorClient(res, 404, errorServicio);
 
@@ -92,20 +70,3 @@ export async function updateServicio(req, res){
     }
 }
 
-export async function deleteServicio(req, res){
-    try{
-        const { id } = req.query;
-
-        const { error } = servicioQuerySchema.validate({ id });
-
-        if(error) return handleErrorClient(res, 400, "Error de validación en la consulta", error.message);
-
-        const [servicio, errorServicio] = await deleteServicioService({ id });
-
-        if(errorServicio) return handleErrorClient(res, 404, errorServicio);
-
-        handleSuccess(res, 200, "Servicio eliminado", servicio);
-    }catch(error){
-        handleErrorServer(res, 500, error.message);
-    }
-}
