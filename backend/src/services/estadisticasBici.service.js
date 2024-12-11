45a83f9e380e0701e8280d39274a91a36fbe3ea8
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-12-11 01:40:02 -0300

    MOD estadisticasBici.service.js con filtro de meses

--- backend/src/services/estadisticasBici.service.js
+++ backend/src/services/estadisticasBici.service.js
@@ -3,56 +3,7 @@ import { AppDataSource } from "../config/configDb.js";
 import Bicicletas from "../entity/bicicleta.entity.js";
 import { IsNull, Not } from "typeorm";
 
-//Servicio para obtener bicicletas por tipo
-export async function getBicicletasPorTipo() {
-    try {
-        const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
-
-        const bicicletasPorTipo = await bicicletaRepository.createQueryBuilder("bicicleta")
-            .select("bicicleta.tipo, COUNT(*) as cantidad")
-            .groupBy("bicicleta.tipo")
-            .getRawMany();
-
-        return [bicicletasPorTipo, null];
-    } catch (error) {
-        console.error("Error al obtener la distribución de bicicletas por tipo:", error);
-        return [null, "Error interno del servidor"];
-    }
-}
-
-//Servicio para obtener bicicletas a la venta
-export async function getBicicletasVenta() {
-    try {
-        const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
-
-        const bicicletasVendidas = await bicicletaRepository.find({
-            where: { venta: Not(IsNull()) },
-            select: ["numeroSerie", "marca", "modelo", "venta"]
-        });
-        return [bicicletasVendidas, null];
-    } catch (error) {
-        console.error("Error al obtener bicicletas a la venta:", error);
-        return [null, "Error interno del servidor"];
-    }
-}
-
-// Servicio para obtener bicicletas por Aro 
-export async function getBicicletasPorAro() {
-    try {
-        const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
-
-        const bicicletasPorAro = await bicicletaRepository.find({
-            select: ["aro", "modelo"]
-        });
-
-        return [bicicletasPorAro, null];
-    } catch (error) {
-        console.error("Error al obtener las bicicletas con aro:", error);
-        return [null, "Error interno del servidor"];
-    }
-}
-
-// Servicio para obtener bicicletas por tipo filtrado por meses
+//Servicio para obtener bicicletas por tipo filtrado por meses
 export async function getBicicletasPorTipoMes(mes) {
     try {
         const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
@@ -66,6 +17,43 @@ export async function getBicicletasPorTipoMes(mes) {
         return [bicicletasPorTipo, null];
     } catch (error) {
         console.error("Error al obtener bicicletas por tipo y mes:", error);
+        return [null, "Error interno del servidor"];
+    }
+}
+
+//Servicio para obtener bicicletas a la venta filtrado por meses
+export async function getBicicletasVentaMes(mes) {
+    try {
+        const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
+
+        const bicicletasVendidasMes = await bicicletaRepository.createQueryBuilder("bicicleta")
+            .select(["bicicleta.modelo", "bicicleta.venta"])
+            .where("EXTRACT(MONTH FROM bicicleta.createdAt) = :mes ", { mes })
+            .andWhere("bicicleta.venta IS NOT NULL")
+            .andWhere("bicicleta.venta > 0")
+            .getMany();
+        
+        return [bicicletasVendidasMes, null];
+    } catch (error) {
+        console.error("Error al obtener bicicletas a la venta por mes:", error);
+        return [null, "Error interno del servidor"];
+    }
+}
+
+// Servicio para obtener bicicletas por Aro filtrado por meses
+export async function getBicicletasPorAroMes(mes) {
+    try {
+        const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
+
+        const bicicletasPorAro = await bicicletaRepository.createQueryBuilder("bicicleta")
+            .select("bicicleta.aro, bicicleta.modelo")
+            .where("EXTRACT(MONTH FROM bicicleta.createdAt) = :mes", { mes })
+            .groupBy("bicicleta.aro, bicicleta.modelo")
+            .getRawMany();
+
+        return [bicicletasPorAro, null];
+    } catch (error) {
+        console.error("Error al obtener las bicicletas con aro y mes:", error);
         return [null, "Error interno del servidor"];
     }
 }
