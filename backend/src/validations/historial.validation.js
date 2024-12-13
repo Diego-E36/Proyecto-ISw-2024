b652f6159af6fd11ae75bd466c617931c78d314d
Ignacio Mora <ignacio.mora2001@alumnos.ubiobio.cl>
2024-12-13 17:50:45 -0300

    ADD: validacion de query historial

---
+++ backend/src/validations/historial.validation.js
@@ -0,0 +1,13 @@
+"use strict"
+import Joi from "joi"
+
+export const histQueryValidation = Joi.object({
+    id: Joi.number()
+        .integer()
+        .positive()
+        .messages({
+            "number.base": "El id debe ser un número.",
+            "number.integer": "El id debe ser un número entero.",
+            "number.positive": "El id debe ser un número positivo.",
+        })
+    })
