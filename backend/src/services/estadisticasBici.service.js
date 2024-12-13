3c81a8eef028464d275c97a39c8e4cf24c5c84df
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-12-13 01:59:45 -0300

    MOD estadisticasBici.service.js, se añaden nuevas funcionalidades para mostrar en el frontend

--- backend/src/services/estadisticasBici.service.js
+++ backend/src/services/estadisticasBici.service.js
@@ -1,7 +1,23 @@
 "use strict";
 import { AppDataSource } from "../config/configDb.js";
 import Bicicletas from "../entity/bicicleta.entity.js";
-import { IsNull, Not } from "typeorm";
+
+
+//Servicio para obtener todas las bicicletas por tipo
+export async function getAllBicicletasTipo() {
+    try {
+        const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
+
+        const allBicicletasTipo = await bicicletaRepository.createQueryBuilder("bicicleta")
+        .select("bicicleta.tipo, COUNT(*) as cantidad")	
+        .groupBy("bicicleta.tipo")
+        .getRawMany();
+        return [allBicicletasTipo, null];
+    } catch (error) {
+        console.error("Error al obtener todas las bicicletas:", error);
+        return [null, "Error interno del servidor"];
+    }
+}
 
 //Servicio para obtener bicicletas por tipo filtrado por meses
 export async function getBicicletasPorTipoMes(mes) {
@@ -17,6 +33,23 @@ export async function getBicicletasPorTipoMes(mes) {
         return [bicicletasPorTipo, null];
     } catch (error) {
         console.error("Error al obtener bicicletas por tipo y mes:", error);
+        return [null, "Error interno del servidor"];
+    }
+}
+
+//Servicio para obtener todas las bicicletas a la venta por modelo
+export async function getAllBicicletasVenta() {
+    try {
+        const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
+
+        const bicicletasVenta = await bicicletaRepository.createQueryBuilder("bicicleta")
+            .select(["bicicleta.modelo", "bicicleta.venta"])
+            .where("bicicleta.venta > 0")
+            .getRawMany();
+
+        return [bicicletasVenta, null];
+    } catch (error) {
+        console.error("Error al obtener todas las bicicletas a la venta:", error);
         return [null, "Error interno del servidor"];
     }
 }
@@ -40,15 +73,32 @@ export async function getBicicletasVentaMes(mes) {
     }
 }
 
-// Servicio para obtener bicicletas por Aro filtrado por meses
+//Servicio para obtener todas las bicicletas por aro
+export async function getAllBicicletasPorAro() {
+    try {
+        const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
+
+        const bicicletasPorAro = await bicicletaRepository.createQueryBuilder("bicicleta")
+            .select("CAST(bicicleta.aro AS VARCHAR) AS aro, COUNT(*) as cantidad")
+            .groupBy("bicicleta.aro")
+            .getRawMany();
+
+        return [bicicletasPorAro, null];
+    } catch (error) {
+        console.error("Error al obtener todas las bicicletas por aro:", error);
+        return [null, "Error interno del servidor"];
+    }
+}
+
+//Servicio para obtener bicicletas por Aro filtrado por meses
 export async function getBicicletasPorAroMes(mes) {
     try {
         const bicicletaRepository = AppDataSource.getRepository(Bicicletas);
 
         const bicicletasPorAro = await bicicletaRepository.createQueryBuilder("bicicleta")
-            .select("bicicleta.aro, bicicleta.modelo")
+        .select("CAST(bicicleta.aro AS VARCHAR) AS aro, COUNT(*) as cantidad")
             .where("EXTRACT(MONTH FROM bicicleta.createdAt) = :mes", { mes })
-            .groupBy("bicicleta.aro, bicicleta.modelo")
+            .groupBy("bicicleta.aro")
             .getRawMany();
 
         return [bicicletasPorAro, null];
