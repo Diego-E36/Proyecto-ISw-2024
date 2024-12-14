ab60e2844993830c79fa5eb8cb6634e31defc93c
Javier González <javier.gonzalez2101@alumnos.ubiobio.cl>
2024-12-14 02:38:56 -0300

    MOD: Buenas prácticas

--- frontend/src/pages/Bicicletas.jsx
+++ frontend/src/pages/Bicicletas.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useMemo } from 'react';
 import Table from '../components/Table';
 import useBicicletas from '../hooks/bicicletas/useGetBicicletas';
 import Search from '../components/Search';
@@ -16,7 +16,6 @@ import useDeleteBicicleta from '../hooks/bicicletas/useDeleteBicicleta';
 import ViewIcon from '../assets/ViewIcon.svg';
 import MoneyIcon from '../assets/MoneyIcon.svg';
 import MoneyOffIcon from '../assets/MoneyOffIcon.svg';
-import { useMemo } from 'react'; // useMemo para evitar recalcular la lista de bicicletas cuando el filtro de valor de venta cambia.
 const Bicicletas = () => {
     const { bicicletas, fetchBicicletas, setBicicletas } = useBicicletas();
     const [filterNumeroSerie, setFilterNumeroSerie] = useState('');
@@ -63,9 +62,9 @@ const Bicicletas = () => {
     // Filtrar bicicletas según el filtro actual de valor de venta
     const filteredBicicletas = useMemo(() => {
         return bicicletas.filter((bicicleta) => {
-            if (ventaFilter === null) return true; // Mostrar todas las bicicletas
-            if (ventaFilter === 0) return bicicleta.venta === "No está a la venta"; // Venta igual a 0
-            if (ventaFilter === 1) return bicicleta.venta !== "No está a la venta"; // Venta distinto de 0
+            if(ventaFilter === null) return true; // Mostrar todas las bicicletas
+            if(ventaFilter === 0) return bicicleta.venta === "No está a la venta"; // Venta igual a 0
+            if(ventaFilter === 1) return bicicleta.venta !== "No está a la venta"; // Venta distinto de 0
             return true;
         });
     }, [bicicletas, ventaFilter]);
