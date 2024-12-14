ab588965e2c7b8aec4d1c45c9078f6555d289f52
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-12-14 13:06:58 -0300

    MOD se agregan nuevas rutas de filtrar por año, filtrar por los últimos tres meses para tipobici, ventabici, arobici

--- backend/src/routes/estadisticasBici.routes.js
+++ backend/src/routes/estadisticasBici.routes.js
@@ -1,7 +1,19 @@
 "use strict";
 import { Router } from "express";
-import {  getAllBicicletasPorAroController, getAllBicicletasTipoController, getAllBicicletasVentaController,  getBicicletasPorAroMesController, 
-getBicicletasPorTipoMesController, getBicicletasVentasMesController } from "../controllers/estadisticasBici.controller.js";
+import {  
+    getAllBicicletasPorAroController,
+    getAllBicicletasTipoController,
+    getAllBicicletasVentaController,
+    getBicicletasPorAroMesController,
+    getBicicletasPorAroUltimosTresMesesController,
+    getBicicletasPorAroYearController,
+    getBicicletasPorTipoAñoController,
+    getBicicletasPorTipoMesController,
+    getBicicletasPorTipoUltimosTresMesesController,
+    getBicicletasVentasMesController,
+    getBicicletasVentaUltimosTresMesesController,
+    getBicicletasVentaYearController
+} from "../controllers/estadisticasBici.controller.js";
 import { authenticateJwt } from "../middlewares/authentication.middleware.js";
 import { isAdmin } from "../middlewares/authorization.middleware.js";
 
@@ -16,9 +28,15 @@ router
 router
     .get("/alltipo", getAllBicicletasTipoController)
     .get("/tipobici/:mes", getBicicletasPorTipoMesController)
+    .get("/tipotresmeses", getBicicletasPorTipoUltimosTresMesesController)
+    .get("/anobici/:year", getBicicletasPorTipoAñoController)
     .get("/allventa", getAllBicicletasVentaController)
     .get("/ventabici/:mes", getBicicletasVentasMesController)
+    .get("/ventatresmeses", getBicicletasVentaUltimosTresMesesController)
+    .get("/anobiciventa/:year", getBicicletasVentaYearController)
     .get("/allaros", getAllBicicletasPorAroController)
-    .get("/arobici/:mes", getBicicletasPorAroMesController);
+    .get("/arobici/:mes", getBicicletasPorAroMesController)
+    .get("/arotresmeses", getBicicletasPorAroUltimosTresMesesController)
+    .get("/anobiciaro/:year", getBicicletasPorAroYearController);
 
 export default router;
