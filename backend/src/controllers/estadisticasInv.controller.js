ac7283b192e717a173655076e69ffb0a0801dbb5
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-12-16 23:04:43 -0300

    MOD se deletea controller de estaciones

--- backend/src/controllers/estadisticasInv.controller.js
+++ backend/src/controllers/estadisticasInv.controller.js
@@ -13,7 +13,6 @@ import { getDistribucionProductosPorProveedor,
         getProductosBajoStockYRestockSugerido,
         } from "../services/estadisticasInv.service.js";
 import { handleErrorClient, handleErrorServer, handleSuccess } from "../handlers/responseHandlers.js";
-//Aquí van las cosas para manejar las peticiones HTTP
 
 // Obtener el nombre del producto y la cantidad
 export async function getNombreYCantidadInventarioController(req, res) {
@@ -304,27 +303,3 @@ export async function getInventarioBajoStockRestockUltimosTresMesesController(re
         handleErrorServer(res, 500, "Error interno del servidor", error);
     }
 }
-
-
-
-
-//Obtiene las estadísticas por estación
-export async function getEstadisticasxEstacionController(req, res) {
-    const { estacion } = req.params;
-    try {
-        const [estadisticas, error] = await getEstadisticasxEstacionService(estacion);
-
-        if (error) {
-            return handleErrorClient(res, 404, "Error al obtener las estadísticas por estación", error);
-        }
-
-        if (!estadisticas || estadisticas.length === 0) {
-            return handleSuccess(res, 204, "No hay estadísticas registradas para la estación especificada");
-        }
-
-        handleSuccess(res, 200, "Estadísticas por estación obtenidas", estadisticas);
-    } catch (error) {
-        handleErrorServer(res, 500, "Error interno del servidor", error);
-    }
-}
-
