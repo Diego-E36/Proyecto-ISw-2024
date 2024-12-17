394c35479c4ccf4a14cfb8b244fb5e998ebe4c50
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-12-16 23:05:29 -0300

    MOD se añaden las nuevas rutas para el filtro de dia/mes/year

--- backend/src/routes/estadisticasBici.routes.js
+++ backend/src/routes/estadisticasBici.routes.js
@@ -4,12 +4,15 @@ import {
     getAllBicicletasPorAroController,
     getAllBicicletasTipoController,
     getAllBicicletasVentaController,
+    getBicicletasPorAroDiaController,
     getBicicletasPorAroMesController,
     getBicicletasPorAroUltimosTresMesesController,
     getBicicletasPorAroYearController,
     getBicicletasPorTipoAñoController,
+    getBicicletasPorTipoDiaController,
     getBicicletasPorTipoMesController,
     getBicicletasPorTipoUltimosTresMesesController,
+    getBicicletasVentaDiaController,
     getBicicletasVentasMesController,
     getBicicletasVentaUltimosTresMesesController,
     getBicicletasVentaYearController
@@ -27,14 +30,17 @@ router
 // Define rutas para las estadísticas de bicicletas
 router
     .get("/alltipo", getAllBicicletasTipoController)
+    .get("/tipobicidia/:dia/:mes/:year", getBicicletasPorTipoDiaController)
     .get("/tipobici/:mes/:year", getBicicletasPorTipoMesController)
     .get("/tipotresmeses", getBicicletasPorTipoUltimosTresMesesController)
     .get("/anobici/:year", getBicicletasPorTipoAñoController)
     .get("/allventa", getAllBicicletasVentaController)
+    .get("/ventabicidia/:dia/:mes/:year", getBicicletasVentaDiaController)
     .get("/ventabici/:mes/:year", getBicicletasVentasMesController)
     .get("/ventatresmeses", getBicicletasVentaUltimosTresMesesController)
     .get("/anobiciventa/:year", getBicicletasVentaYearController)
     .get("/allaros", getAllBicicletasPorAroController)
+    .get("/arobicidia/:dia/:mes/:year", getBicicletasPorAroDiaController)
     .get("/arobici/:mes/:year", getBicicletasPorAroMesController)
     .get("/arotresmeses", getBicicletasPorAroUltimosTresMesesController)
     .get("/anobiciaro/:year", getBicicletasPorAroYearController);
